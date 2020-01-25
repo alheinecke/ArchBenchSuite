967066600cdfdcc23eaff07b119846c3a459c3e6
Alexander Heinecke <alexander.heinecke@intel.com>
2020-01-25 10:46:45 -0800

    updated run scripts

--- level1/sgemm/run_dual_socket.sh
+++ level1/sgemm/run_dual_socket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1st socket
-OMP_NUM_THREADS=18 KMP_AFFINITY=proclist=[0-17],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 10 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[0-27],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[28-55],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
 
-# 2nd socket
-OMP_NUM_THREADS=18 KMP_AFFINITY=proclist=[18-35],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 10 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[0-27],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[28-55],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
 
--- level1/sgemm/run_octo_socket.sh
+++ level1/sgemm/run_octo_socket.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[0-27],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[28-55],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[56-83],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[84-111],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[112-139],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[140-167],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[168-195],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
-OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[196-223],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[0-27],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[28-55],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[56-83],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[84-111],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[112-139],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[140-167],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[168-195],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+#OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[196-223],granularity=fine,explicit,norespect LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[0-27],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[28-55],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[56-83],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[84-111],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[112-139],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[140-167],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[168-195],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
+OMP_NUM_THREADS=28 KMP_AFFINITY=proclist=[196-223],granularity=fine,explicit,norespect ./sgemm.exe 9000 9000 9000 n n 10 1000 &
 
